9fcb663e1d82292eefe393484a9cd7733d605a35
uhsandrea <74327083+uhsandrea@users.noreply.github.com>
2021-06-13 23:06:23 -0700

    Files setup

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <ul className="nav-bar">
+      <li>Home</li>
+      <li>About</li>
+      <li>Projects</li>
+      <li>Contact</li>
+    </ul>
+  );
+}
+
+export default Nav;
