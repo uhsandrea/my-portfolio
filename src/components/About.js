fc3a61ffac38f6d76ef53bfb0fe2a7a4c0e9d7ec
uhsandrea <74327083+uhsandrea@users.noreply.github.com>
2021-06-19 00:19:41 -0700

    Update About

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const About = () => {
+  const [skills, setSkills] = useState(
+    ["HTML5", "CSS3/SASS", "JavasSript", "JSON", "React JS", "Redux", "Light Python/Flask", "Git/GitHub", "Responsive Web Design"]
+  );
+
   return (
     <section className="about-container">
-      About
+      <div className="aboutme">
+        <h1>About Me</h1>
+        <h3>
+          Hi! I am a self-taught front-end developer with solid foundation.
+          I want to learn and gain real-world experience.
+          I am looking for any opportunity of entry level/junior front-end web developer position and 
+          ready to grow from the bottom to become a professional web developer.
+          <br /><br />
+          I will love to hear from you. Feel free to contact me.
+        </h3>
+      </div>
+      <div className="skills">
+        <h2>My Skills</h2>
+        <div>
+          {skills.map(skill => {
+            return <span>{skill}</span>
+          })}
+        </div>
+      </div>
+
     </section>
   );
 }
